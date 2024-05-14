ef8346865657fc3b8d20c97f43cecbdf0869f799
hijjoy <hijjoy@naver.com>
2024-05-14 20:55:28 +0900

    chore: main 모바일 화면

--- fitting-pair/src/constants/size.js
+++ fitting-pair/src/constants/size.js
@@ -1,6 +1,6 @@
 const RESPONSIVE_SIZE = {
-  // MOBILE: "600px",
-  // TABLET: "768px",
+  MOBILE: "600px",
+  TABLET: "768px",
   IPAD_PRO: "1024px",
 };
 
--- fitting-pair/src/pages/Home/Homepage.jsx
+++ fitting-pair/src/pages/Home/Homepage.jsx
@@ -1,8 +1,11 @@
 import * as S from "./Homepage.style";
 import LOGO from "../../assets/images/Logo.webp";
 import START_IMG from "../../assets/images/start_logo.png";
+import { useNavigate } from "react-router-dom";
 
 const Homepage = () => {
+  const nav = useNavigate();
+
   return (
     <S.Container>
       <S.LogoWrapper>
@@ -14,8 +17,7 @@ const Homepage = () => {
           recommends clothing through CV and AI.
         </p>
       </S.Text>
-
-      <button>
+      <button onClick={() => nav("/sign-up")}>
         START
         <img src={START_IMG} />
       </button>
