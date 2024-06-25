9649f8b24afb712663578d620f8e1851128e4eaa
hijjoy <hijjoy@naver.com>
2024-06-25 18:37:44 +0900

    chore: 상단 Icon 컴포넌트로 분리 및 라우터 수정

--- fitting-pair/src/components/index.js
+++ fitting-pair/src/components/index.js
@@ -2,5 +2,7 @@ import Submit from "./Button/Submit";
 import Input from "./Input/Input";
 import Loading from "./Loading/Loading";
 import Webcam from "./Webcam/Webcam";
+import Modal from "./Modal/Modal";
+import Icon from "./Icon/Icon";
 
-export { Submit, Input, Loading, Webcam };
+export { Submit, Input, Loading, Webcam, Modal, Icon };
--- fitting-pair/src/pages/Styling/StylingPage.jsx
+++ fitting-pair/src/pages/Styling/StylingPage.jsx
@@ -1,18 +1,12 @@
 import * as S from "./StylingPage.style";
-import checkImg from "../../assets/images/check.png";
-import clothImg from "../../assets/images/cloth-white.png";
-import penImg from "../../assets/images/pen-black.png";
-import tipImg from "../../assets/images/tip.png";
 import topImg from "../../assets/images/top.png";
 import resultImg from "../../assets/images/result.png";
-import Webcam from "../../components/Webcam/Webcam";
-import { Submit } from "../../components";
+import { Icon } from "../../components";
 import Modal from "../../components/Modal/Modal";
-import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
+// result 페이지
 const StylingPage = () => {
-  const nav = useNavigate();
   const [isModalVisible, setModalVisible] = useState(false);
 
   const openModal = () => {
@@ -25,60 +19,55 @@ const StylingPage = () => {
 
   return (
     <S.Container>
-      <S.IconWrapper>
-        <S.Icon>
-          <img src={penImg} />
-          Body Check
-        </S.Icon>
-        <S.IconBlack>
-          <img src={clothImg} />
-          Styling
-        </S.IconBlack>
-        <S.Icon>
-          <img src={checkImg} />
-          Result
-        </S.Icon>
-      </S.IconWrapper>
-
+      <Icon blackNum={3} />
       <S.ContentResultContainer>
         <S.ResultWrapper>
-          <S.ResultTitle> 
+          <S.ResultTitle>
             YOU ARE <span>SQUARE</span> BODY.
           </S.ResultTitle>
-          <S.ResultImg>            
-          <img src={resultImg} />
+          <S.ResultImg>
+            <img src={resultImg} />
           </S.ResultImg>
           <S.ResultExplainWrapper>
             <S.ContentWrapper>
-              <span>
-              FEATURES
-              </span>
+              <span>FEATURES</span>
               <p>
-              consectetur. Vestibulum sit facilisis purus orci commodo ornare. Donec fermentum urna sit sed est cras faucibus in et. Eget habitasse vitae in in dictum viverra velit. Lorem morbi massa neque massa adipiscing pharetra facilisi sagittis viverra. Aliquam nec volutpat nulla aenean. Sed sollicitudin odio felis imperdiet faucibus quis mattis porttitor. Duis vehicula aliquet dui nulla sagittis potenti purus dignissim. Lacus dictum eget pellentesque quis nunc lectus. Dignissim amet ultricies velit etiam aenean.
+                consectetur. Vestibulum sit facilisis purus orci commodo ornare. Donec fermentum urna sit sed est cras
+                faucibus in et. Eget habitasse vitae in in dictum viverra velit. Lorem morbi massa neque massa
+                adipiscing pharetra facilisi sagittis viverra. Aliquam nec volutpat nulla aenean. Sed sollicitudin odio
+                felis imperdiet faucibus quis mattis porttitor. Duis vehicula aliquet dui nulla sagittis potenti purus
+                dignissim. Lacus dictum eget pellentesque quis nunc lectus. Dignissim amet ultricies velit etiam aenean.
               </p>
-            </S.ContentWrapper>  
+            </S.ContentWrapper>
             <S.ContentWrapper>
-            <span>
-              FEATURES
-              </span>
+              <span>FEATURES</span>
               <p>
-              consectetur. Vestibulum sit facilisis purus orci commodo ornare. Donec fermentum urna sit sed est cras faucibus in et. Eget habitasse vitae in in dictum viverra velit. Lorem morbi massa neque massa adipiscing pharetra facilisi sagittis viverra. Aliquam nec volutpat nulla aenean. Sed sollicitudin odio felis imperdiet faucibus quis mattis porttitor. Duis vehicula aliquet dui nulla sagittis potenti purus dignissim. Lacus dictum eget pellentesque quis nunc lectus. Dignissim amet ultricies velit etiam aenean.
+                consectetur. Vestibulum sit facilisis purus orci commodo ornare. Donec fermentum urna sit sed est cras
+                faucibus in et. Eget habitasse vitae in in dictum viverra velit. Lorem morbi massa neque massa
+                adipiscing pharetra facilisi sagittis viverra. Aliquam nec volutpat nulla aenean. Sed sollicitudin odio
+                felis imperdiet faucibus quis mattis porttitor. Duis vehicula aliquet dui nulla sagittis potenti purus
+                dignissim. Lacus dictum eget pellentesque quis nunc lectus. Dignissim amet ultricies velit etiam aenean.
               </p>
-            </S.ContentWrapper>  
-            </S.ResultExplainWrapper>
-            <S.ClothWrapper>
-              <span>SELECTED APPAREL</span>
-              <section>
-                <div><img src={topImg} /></div>
-                <div><img src={topImg} /></div>
-                <div><img src={topImg} /></div>
-              </section>
-            </S.ClothWrapper>
+            </S.ContentWrapper>
+          </S.ResultExplainWrapper>
+          <S.ClothWrapper>
+            <span>SELECTED APPAREL</span>
+            <section>
+              <div>
+                <img src={topImg} />
+              </div>
+              <div>
+                <img src={topImg} />
+              </div>
+              <div>
+                <img src={topImg} />
+              </div>
+            </section>
+          </S.ClothWrapper>
 
-            <S.Button onClick={openModal}>Store</S.Button>
-            <Modal isVisible={isModalVisible} onClose={closeModal} />
-
-        </S.ResultWrapper>        
+          <S.Button onClick={openModal}>Store</S.Button>
+          <Modal isVisible={isModalVisible} onClose={closeModal} />
+        </S.ResultWrapper>
       </S.ContentResultContainer>
     </S.Container>
   );
