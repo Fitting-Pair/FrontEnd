09e41c4d72823bfc9a3b9fbecf229b3d2253724d
hijjoy <hijjoy@naver.com>
2024-06-25 16:59:26 +0900

    remove: resultLayout 삭제

--- fitting-pair/src/layout/ResultLayout/ResultLayout.style.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const Container = styled.div`
-  background-color: #f4efe7;
-  width: 100vw;
-  height: calc(var(--vh, 1vh) * 172);
-`;
-
-export { Container };
