28c3f3c065ec69a0be8122e3945e0574a02a6e68
hyewoniiiii <hyewon2466@gmail.com>
2024-06-28 01:44:56 +0900

    feat: 마이페이지 구현...하려고 노력한...

--- fitting-pair/src/pages/MyPage/MyPage.jsx
+++ fitting-pair/src/pages/MyPage/MyPage.jsx
@@ -1,7 +1,64 @@
 import * as S from "./MyPage.style";
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+import { useNavigate } from "react-router-dom";
+import Logout from "../../assets/images/logout.png"
+import Logo from "../../assets/images/FITTINGPAIR-black.png"
+
+// 모바일 myPage
 const MyPage = () => {
-  return <S.Container>마이페이지</S.Container>;
+  const nav = useNavigate();
+
+  return (
+    <S.Container>
+      <S.ContentMyPageContainer>
+        <span>MyPage</span>
+
+        <S.LogoutWrapper>
+            <S.LogoutIcon>
+              <img src={Logout} />
+            </S.LogoutIcon>
+        </S.LogoutWrapper>
+
+        <S.MyResultWrapper>
+          <S.MyResultTitle>
+            PREVIOUS
+            RESULT
+          </S.MyResultTitle>
+
+          <Swiper
+            slidesPerView={3}
+            spaceBetween={30}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Pagination]}
+            className="mySwiper"
+          >
+            <SwiperSlide>Slide 1</SwiperSlide>
+            <SwiperSlide>Slide 2</SwiperSlide>
+            <SwiperSlide>Slide 3</SwiperSlide>
+            <SwiperSlide>Slide 4</SwiperSlide>
+            <SwiperSlide>Slide 5</SwiperSlide>
+          </Swiper>
+        </S.MyResultWrapper>
+
+        <S.MyPageDate>
+            May, 13th
+        </S.MyPageDate>
+        
+        <S.BlackLogo src={Logo} />
+      </S.ContentMyPageContainer>
+    </S.Container>
+  );
 };
 
 export default MyPage;
--- fitting-pair/src/pages/MyPage/MyPage.style.js
+++ fitting-pair/src/pages/MyPage/MyPage.style.js
@@ -8,4 +8,107 @@ const Container = styled.div`
   background: linear-gradient(20deg, #b0cfb2, #f6ae7c);
 `;
 
-export { Container };
+const MyResultTitle = styled.div`
+  ${theme.ALIGN.COLUMN_CENTER};
+  font-family: ${theme.FONT.JIMHURIA_REGULAR};
+  font-size: 128px;
+
+  span {
+    color: #000;
+  }
+`;
+
+const ContentMyPageContainer = styled.div`
+  position: relative;
+  display: grid;
+  place-items: center;
+`
+
+const LogoutWrapper = styled.div`
+
+  display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    margin-top: 40px;
+    margin-right: 40px;
+
+    @media only screen and (min-device-width: 1024px) and (max-device-width: 1366px) {
+      margin-top: 70px;
+    }
+
+    @media only screen and (min-device-width: 768px) and (max-device-width: 1023px) {
+      margin-top: 55px;
+    }
+
+`
+
+const LogoutIcon = styled.div`
+
+  @media only screen and (min-device-width: 1024px) and (max-device-width: 1366px) {
+    width: 50px;
+    height: 50px;
+    margin-top: 50px;
+    margin-right: 50px;
+
+    img {
+      width: 23px;
+    }
+  }
+
+  @media only screen and (min-device-width: 768px) and (max-device-width: 1023px) {
+    width: 30px;
+    height: 30px;
+    margintop: 30px;
+    marginright: 30px;
+
+    img {
+      width: 16px;
+    }
+
+`
+
+const MyResultWrapper = styled.div`
+
+`
+
+const Swiper = styled.div`
+  width: 100%;
+  height: 100%;
+`
+
+const SwiperSlide = styled.div`
+  ${theme.ALIGN.COLUMN_CENTER};
+  font-size: 18px;
+  background: #fff;
+
+  /* Center slide text vertically */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const MyPageDate = styled.div`
+  ${theme.ALIGN.COLUMN_LEFT};
+  color: #fff;
+  width: 530px;
+  margin-left: 30px;
+  margin-top: 15px;
+
+  font-family: ${theme.FONT.ROBOTO_BOLD};
+  
+`
+
+const BlackLogo = styled.img`
+  ${theme.ALIGN.COLUMN_CENTER};
+
+  margin-bottom: 50px;
+
+  @media ${theme.WINDOW_SIZE.MOBILE} {
+    width: 90%;
+
+`
+
+export { Container, ContentMyPageContainer, LogoutWrapper, LogoutIcon, MyResultWrapper, MyResultTitle, Swiper, SwiperSlide, MyPageDate, BlackLogo };
+
