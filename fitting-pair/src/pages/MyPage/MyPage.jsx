baa1d24f1566118c55c2d861ff07fb9885be1733
hijjoy <hijjoy@naver.com>
2024-07-01 00:44:08 +0900

    chore: 마이페이지 화면 ui 수정

--- fitting-pair/src/pages/MyPage/MyPage.jsx
+++ fitting-pair/src/pages/MyPage/MyPage.jsx
@@ -1,45 +1,24 @@
 import * as S from "./MyPage.style";
+import Logout from "../../assets/images/logout.png";
+import Logo from "../../assets/images/FITTINGPAIR-black.png";
 
-// import required modules
-import { useNavigate } from "react-router-dom";
-import Logout from "../../assets/images/logout.png"
-import Logo from "../../assets/images/FITTINGPAIR-black.png"
-
-// 모바일 myPage
 const MyPage = () => {
-  const nav = useNavigate();
-
   return (
     <S.Container>
-      <S.ContentMyPageContainer>
-        <S.MyPageWrapper>
-          <S.MyPage>
-            <div style={{ alignSelf: 'center' }}>
-              MYPAGE
-            </div>
-              <button onClick={MyPage}>
-                <img src={Logout} />
-              </button>
-          </S.MyPage>
-        </S.MyPageWrapper>
-
-        <S.MyResultWrapper>
-          <S.MyResultTitle>
-            PREVIOUS <br />
-            RESULT
-          </S.MyResultTitle>
-        </S.MyResultWrapper>
-
-        <S.PreviousResultWrapper>
-
-        </S.PreviousResultWrapper>
-
-        <S.MyPageDate>
-            May, 13th
-        </S.MyPageDate>
-        
-        <S.BlackLogo src={Logo} />
-      </S.ContentMyPageContainer>
+      <S.MyPageWrapper>
+        <span></span>
+        <div>MYPAGE</div>
+        <img src={Logout} />
+      </S.MyPageWrapper>
+      <S.MyResultTitle>
+        PREVIOUS <br />
+        RESULT
+      </S.MyResultTitle>
+      <S.PreviousResultWrapper>
+        <S.PreviousData />
+        <S.MyPageDate>May, 13th</S.MyPageDate>
+      </S.PreviousResultWrapper>
+      <S.BlackLogo src={Logo} />
     </S.Container>
   );
 };
--- fitting-pair/src/pages/MyPage/MyPage.style.js
+++ fitting-pair/src/pages/MyPage/MyPage.style.js
@@ -2,124 +2,83 @@ import styled from "styled-components";
 import theme from "../../styles/theme";
 
 const Container = styled.div`
-  ${theme.ALIGN.COLUMN_CENTER};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   width: 100%;
   height: 100%;
   background: linear-gradient(20deg, #b0cfb2, #f6ae7c);
-`;
-
-const MyResultTitle = styled.div`
-  ${theme.ALIGN.COLUMN_CENTER};
-  font-family: ${theme.FONT.JIMHURIA_REGULAR};
-  font-size: 128px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  line-height:70px;
-  border-radius: 16px;
-  margin: 10px;
-  margin-top: 30px;
-
-  span {
-    text-align: center;
-    color: #000;
-  }
-`;
-
-const ContentMyPageContainer = styled.div`
-  position: relative;
-  display: grid;
-  place-items: center;
+  overflow: hidden;
 `;
 
 const MyPageWrapper = styled.div`
-  ${theme.ALIGN.COLUMN_CENTER};
-  width: 400px;
-  height: 50px;
-`;
-
-const MyPage = styled.div`
-
+  width: 100%;
+  position: relative;
+  top: 25px;
   display: flex;
-
-  margin: 10px 30px;
+  justify-content: space-around;
   font-size: 16px;
   font-family: ${theme.FONT.ROBOTO_MEDIUM};
 
-  div {
-    ${theme.ALIGN.COLUMN_CENTER};
+  span {
+    width: 28px;
+    height: 30px;
+  }
 
-    position: relative;
-    top: 0.5px;
-    left: 20px;
+  div {
+    ${theme.ALIGN.ROW_CENTER};
     width: 100px;
     height: 30px;
     background-color: white;
     margin: 0px 15px;
     border-radius: 24px;
-    place-items: center;
-    justify-content: center;
-    align-items: center;
   }
 
-  button {
-    ${theme.ALIGN.COLUMN_CENTER};
-    position: relative;
-    top: 0.5px;
-    left: 120px;
-    width: 400px;
-    margin-right: 10px;
-    margin-left: 0px;
-    border: none;
-    width: 33px;
-    height: 33px;
+  img {
     background-color: white;
-    border-radius: 20px;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    img {
-      margin-left: 5px;
-    }
-
-`
-
-const MyResultWrapper = styled.div`
-
+    border-radius: 50%;
+    padding: 8px;
+  }
 `;
 
-const MyPageDate = styled.div`
-  ${theme.ALIGN.COLUMN_LEFT};
-  font-family: ${theme.FONT.ROBOTO_MEDIUM};
-  
-  color: #fff;
-  width: 400px;
-  margin-left: 50px;
-  margin-top: 15px;
-  display: flex;
-  justify-content: flex-start;
-
+const MyResultTitle = styled.div`
+  padding-top: 60px;
+  ${theme.ALIGN.COLUMN_CENTER};
+  font-family: ${theme.FONT.JIMHURIA_REGULAR};
+  font-size: 128px;
+  text-align: center;
+  line-height: 70px;
 `;
 
 const BlackLogo = styled.img`
-  ${theme.ALIGN.COLUMN_CENTER};
-  margin-top: 30px;
-
-  @media ${theme.WINDOW_SIZE.MOBILE} {
-    width: 80%;
-
+  position: fixed;
+  bottom: -10px;
+  @media (max-height: 740px) {
+    bottom: -30px;
+    width: 90%;
+  }
 `;
 
 const PreviousResultWrapper = styled.div`
-    ${theme.ALIGN.COLUMN_CENTER};
-    width: 360px;
-    height: 450px;
-    background-color: white;
-    margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 80%;
+  height: 50%;
 `;
 
-export { Container, ContentMyPageContainer, MyPageWrapper, MyPage, MyResultWrapper, MyResultTitle, MyPageDate, BlackLogo, PreviousResultWrapper };
+const PreviousData = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  margin-top: 20px;
+`;
 
+const MyPageDate = styled.div`
+  padding-top: 10px;
+  width: 100%;
+  font-family: ${theme.FONT.ROBOTO_MEDIUM};
+  color: #fff;
+`;
+
+export { Container, PreviousData, MyPageWrapper, MyResultTitle, MyPageDate, BlackLogo, PreviousResultWrapper };
