76c865ba36ca376c9b62ab39982a7de90ad0a5bd
hijjoy <hijjoy@naver.com>
2024-08-11 00:44:45 +0900

    chore: 로그아웃 수정

--- fitting-pair/src/components/Icon/MobileIcon/MoblieIcon.jsx
+++ fitting-pair/src/components/Icon/MobileIcon/MoblieIcon.jsx
@@ -1,41 +1,15 @@
 import * as S from './MovblieIcon.style';
 import { FaArrowLeft } from 'react-icons/fa6';
 import LogOut from '../../../assets/images/door.png';
+import useLogout from '../../../hooks/queries/useLogout';
 import { useNavigate } from 'react-router-dom';
-import { useMutation } from '@tanstack/react-query';
-import { logout } from '../../../api/user';
-import { toast } from 'sonner';
-import { removeHeader } from '../../../util';
 
 const MoblieIcon = ({ text, noBack }) => {
 	const nav = useNavigate();
-
-	const { mutate } = useMutation({
-		mutationFn: logout,
-		onSuccess: () => {
-			toast.success('로그아웃 되셨습니다.', {
-				duration: 1200,
-			});
-			localStorage.clear();
-			removeHeader('Authorization');
-			removeHeader('Refresh');
-
-			nav('/', { replace: true });
-		},
-		onError: error => {
-			console.log(error);
-			error.response &&
-				toast.error(error.response.data.message, {
-					style: {
-						color: '#fff',
-						background: '#e05151',
-					},
-				});
-		},
-	});
+	const { mutate } = useLogout();
 
 	const handleLogout = () => {
-		mutate({});
+		mutate();
 	};
 
 	return (
