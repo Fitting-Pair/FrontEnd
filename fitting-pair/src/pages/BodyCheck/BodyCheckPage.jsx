3941d765a5de9a2d4db7167f3519bd975d625fa5
hijjoy <hijjoy@naver.com>
2024-08-26 13:57:10 +0900

    chore: 촬영 문구 한글화

--- fitting-pair/src/pages/BodyCheck/BodyCheckPage.jsx
+++ fitting-pair/src/pages/BodyCheck/BodyCheckPage.jsx
@@ -15,14 +15,11 @@ const BodyCheckPage = () => {
 						Tips
 					</S.TipIcon>
 					<S.ContentWrapper>
-						<span>
-							You can get more accurate results
-							<br /> by uploading photos that meet the criteria below.
-						</span>
-						<li>Photo taken from the front</li>
-						<li>Expressionless photo without smiling</li>
-						<li>A photo where the forehead is clearly visible</li>
-						<li>photo without glasses</li>
+						<span>촬영 버튼을 누르고 5초 후에 촬영됩니다.</span>
+						<li>1. 정면을 바라보고 사진을 찍어주세요.</li>
+						<li>2. 옆구리가 붙지 않도록 팔을 벌려주세요.</li>
+						<li>3. 어깨넓이 만큼 다리를 벌려주세요.</li>
+						<li>4. 충분히 거리를 두고 발끝까지 나오게 찍어주세요.</li>
 					</S.ContentWrapper>
 				</S.TipWrapper>
 			</S.ContentContainer>
