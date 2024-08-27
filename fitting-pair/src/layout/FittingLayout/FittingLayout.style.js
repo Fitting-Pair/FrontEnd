4f18f8ad1efd04d22d76f88d5ef9816223559ee3
hyewon <89793335+hijjoy@users.noreply.github.com>
2024-08-27 13:13:39 +0900

    [Sytle] style/#44 배경 선 추가 (#46)

--- fitting-pair/src/layout/FittingLayout/FittingLayout.style.js
+++ fitting-pair/src/layout/FittingLayout/FittingLayout.style.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
+	position: relative;
 	background: linear-gradient(
 		200deg,
 		#fdb985 11.63%,
@@ -11,4 +12,19 @@ const Container = styled.div`
 	min-height: 100dvh;
 `;
 
-export { Container };
+const OutletWrapper = styled.div`
+	position: relative;
+	width: 100%;
+	height: 100%;
+`;
+
+const BackGround = styled.img`
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	object-fit: fill;
+`;
+
+export { Container, OutletWrapper, BackGround };
